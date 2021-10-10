310e4c9624f7174adc41333f0313d038f675fc3c
Carlos Vasquez <cevasquezf22@gmail.com>
2021-10-09 20:32:16 -0500

    Creando searBar

--- src/index.js
+++ src/index.js
@@ -4,3 +4,9 @@
  **/
 
 console.log('Happy hacking :)')
+// const apiKey = f909614ec47c0514210c5a1dcc829f6f;
+const baseUrl = "api.openweathermap.org/data/2.5/weather?q="
+// {city name}&appid={API key}
+
+const city = document.getElementById("buttonSearchCity");
+debugger;
