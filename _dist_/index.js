2b4e7aa3119fa5b14500499ea3d9d786963f517b
CarlosVF22 <CarlosVF22@users.noreply.github.com>
2021-10-10 01:33:08 +0000

    deploy: 310e4c9624f7174adc41333f0313d038f675fc3c

---
+++ _dist_/index.js
@@ -0,0 +1,12 @@
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
+
+console.log('Happy hacking :)')
+// const apiKey = f909614ec47c0514210c5a1dcc829f6f;
+const baseUrl = "api.openweathermap.org/data/2.5/weather?q="
+// {city name}&appid={API key}
+
+const city = document.getElementById("buttonSearchCity");
+debugger;
